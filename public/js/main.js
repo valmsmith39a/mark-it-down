19c85d0f59525e9a3d4f2e2a6beca8f6d3ff8aa3
valmsmith39a <valmsmith39a@gmail.com>
2016-01-25 11:07:04 -0800

    Added real-time/debounce

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,11 @@ $(document).ready(init);
 
 function init(){
   $('#submit-text-btn').on('click', submitTextBtn);
+  $( "#input-text" ).on('keyup', debounce(submitTextBtn, 250));
 }
 
-function submitTextBtn(){
+function submitTextBtn(){	  	
+	$('#output-text').empty();
   var markdownInput = $('#input-text').val();
   $.post('/markdowns', {markdowntext:markdownInput})
   .success(function(data) {
@@ -14,4 +16,16 @@ function submitTextBtn(){
   });
 }
 
-
+function debounce(func, delay) {
+	var timeout; 
+	return function() {
+		var later = () => {
+			timeout = null; 
+			// arguments is an object of arguments passed in. not an array. 
+			func.apply(this, arguments);
+		};
+		clearTimeout(timeout);
+		// reference to the timer set 
+		timeout = setTimeout(later, delay);
+	};
+}
